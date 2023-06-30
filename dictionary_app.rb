2f6718ce0150f112c39a8ec1e12b6d5d4385f1b3
enoch <enokkang@gmail.com>
2023-06-30 13:44:01 -0500

    adds loop and audio

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,14 +1,36 @@
 require "http"
 
-print "Enter a word : "
-word = gets.chomp
+#system("open http://google.com")
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=API-KEY")
+while true
+  print "Enter a word : "
+  word = gets.chomp
 
-#pp response.parse(:json)
+  if word == "q"
+    break
+  end
 
-dictionary_data = response.parse(:json)
-text = dictionary_data[1]["text"]
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=API-KEY")
 
-#pp text
-puts "#{word} : #{text}"
+  #pp response.parse(:json)
+
+  dictionary_data = response.parse(:json)
+  text = dictionary_data[1]["text"]
+
+  response2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=50&api_key=API-KEY")
+
+  #pp response
+  audio_data = response2.parse(:json)
+  #pp audio_data
+
+  audio = audio_data[0]["fileUrl"]
+
+  #pp audio_data
+  #pp text
+  puts "#{word} : #{text}"
+
+  #pp text
+  puts "Pronounciation link : "
+  #puts audio
+  system("open", audio) #{audio}"
+end
